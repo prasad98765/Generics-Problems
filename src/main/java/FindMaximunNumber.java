d9dda529e01d5b0aed6b1681797ba0239bb3828e
prasad98765 <pnchaudhari1996@gmail.com>
2020-01-02 17:25:35 +0530

    REFACTORED : Main To Check Maximum Position For Int,Float,String Type By Using One Generic Method

--- src/main/java/FindMaximunNumber.java
+++ src/main/java/FindMaximunNumber.java
@@ -1,29 +1,7 @@
 public class FindMaximunNumber {
 
-    public Integer FindMaxNumber(Integer a, Integer b, Integer c) {
-        Integer maxNumber = a;
-        if (b.compareTo(maxNumber) > 0){
-            maxNumber=b;
-        }
-        if (c.compareTo(maxNumber) > 0){
-            maxNumber=c;
-        }
-        return maxNumber;
-    }
-
-    public Float FindFloatMaxNumber(Float a, Float b, Float c) {
-        Float maxNumber = a;
-        if (b.compareTo(maxNumber) > 0){
-            maxNumber=b;
-        }
-        if (c.compareTo(maxNumber) > 0){
-            maxNumber=c;
-        }
-        return maxNumber;
-    }
-
-    public String FindStringMax(String a, String b, String c) {
-        String maxNumber = a;
+    public static <E extends Comparable> E FindMaximum(E a,E b,E c) {
+        E maxNumber = a;
         if (b.compareTo(maxNumber) > 0){
             maxNumber=b;
         }
--- src/test/java/TestMaximumNumber.java
+++ src/test/java/TestMaximumNumber.java
@@ -5,21 +5,21 @@ public class TestMaximumNumber {
     @Test
     public void whenGiven1stPosition_ShouldReturn1stPositionMaximum() {
         FindMaximunNumber t1=new FindMaximunNumber();
-        int maxNumber=t1.FindMaxNumber(30,20,10);
+        int maxNumber=t1.FindMaximum(30,20,10);
         Assert.assertEquals(30,maxNumber);
     }
 
    @Test
    public void whenGiven2ndPosition_ShouldReturn2ndPositionMaximum() {
        FindMaximunNumber t1=new FindMaximunNumber();
-       int maxNumber=t1.FindMaxNumber(20,30,10);
+       int maxNumber=t1.FindMaximum(20,30,10);
        Assert.assertEquals(30,maxNumber);
    }
 
    @Test
    public void whenGiven3rdPosition_ShouldReturn3rdPositionMaximum() {
        FindMaximunNumber t1=new FindMaximunNumber();
-       int maxNumber=t1.FindMaxNumber(20,10,30);
+       int maxNumber=t1.FindMaximum(20,10,30);
        Assert.assertEquals(30,maxNumber);
 
     }
@@ -27,42 +27,42 @@ public class TestMaximumNumber {
     @Test
     public void whenGiven1stFloatPosition_ShouldReturn1stPositionMaximum() {
         FindMaximunNumber t1=new FindMaximunNumber();
-        float maxNumber=t1.FindFloatMaxNumber(30.1f,20.2f,10.3f);
+        float maxNumber=t1.FindMaximum(30.1f,20.2f,10.3f);
         Assert.assertEquals(30.1f,maxNumber,0.0);
     }
 
     @Test
     public void whenGiven2stFloatPosition_ShouldReturn1stPositionMaximum() {
         FindMaximunNumber t1=new FindMaximunNumber();
-        float maxNumber=t1.FindFloatMaxNumber(20.1f,30.2f,10.3f);
+        float maxNumber=t1.FindMaximum(20.1f,30.2f,10.3f);
         Assert.assertEquals(30.2f,maxNumber,0.0);
     }
 
     @Test
     public void whenGiven3stFloatPosition_ShouldReturn1stPositionMaximum() {
         FindMaximunNumber t1=new FindMaximunNumber();
-        float maxNumber=t1.FindFloatMaxNumber(20.1f,10.2f,30.3f);
+        float maxNumber=t1.FindMaximum(20.1f,10.2f,30.3f);
         Assert.assertEquals(30.3f,maxNumber,0.0);
     }
 
     @Test
     public void whenGiven1stStringPosition_ShouldReturn1stPositionMaximum() {
         FindMaximunNumber t1=new FindMaximunNumber();
-        String maxString=t1.FindStringMax("peach","apple","banana");
+        String maxString=t1.FindMaximum("peach","apple","banana");
         Assert.assertEquals("peach",maxString);
     }
 
     @Test
     public void whenGiven2stStringPosition_ShouldReturn1stPositionMaximum() {
         FindMaximunNumber t1=new FindMaximunNumber();
-        String maxString=t1.FindStringMax("Apple","peach","banana");
+        String maxString=t1.FindMaximum("Apple","peach","banana");
         Assert.assertEquals("peach",maxString);
     }
 
     @Test
     public void whenGiven3stStringPosition_ShouldReturn1stPositionMaximum() {
         FindMaximunNumber t1=new FindMaximunNumber();
-        String maxString=t1.FindStringMax("banana","apple","peach");
+        String maxString=t1.FindMaximum("banana","apple","peach");
         Assert.assertEquals("peach",maxString);
     }
 
