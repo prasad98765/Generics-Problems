57d21ead672a3d276aa5bfcc32a407e88a953059
prasad98765 <pnchaudhari1996@gmail.com>
2020-01-03 10:57:49 +0530

    MODIFIED : Make Constructor Generic To Pass More Then 3 Parameter

--- src/main/java/FindMaximunNumber.java
+++ src/main/java/FindMaximunNumber.java
@@ -1,27 +1,23 @@
 import java.util.Arrays;
 
 public class FindMaximunNumber<E extends Comparable> {
-     E a,b,c;
+     E[] Array;
 
-    public FindMaximunNumber(E a, E b, E c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    public FindMaximunNumber(E... value) {
+        this.Array=value;
     }
-    public FindMaximunNumber()
-    {
+    public FindMaximunNumber() {
 
     }
 
     public static <E extends Comparable> E FindMaximum(E... value) {
-
         E[] Array=value;
         Arrays.sort(value);
         return value[(value.length)-1];
     }
 
     public Object FindMaximunNumber() {
-        E maxPosition=FindMaximum(a,b,c);
-        return maxPosition;
+        E maxPositionValue=FindMaximum(Array);
+        return maxPositionValue;
     }
 }
--- src/test/java/TestMaximumNumber.java
+++ src/test/java/TestMaximumNumber.java
@@ -68,7 +68,7 @@ public class TestMaximumNumber {
 
     @Test
     public void whenGivenPosition_ShouldReturn1stPositionMaximum() {
-        FindMaximunNumber t1=new FindMaximunNumber(20,10,40);
+        FindMaximunNumber t1=new FindMaximunNumber(40,10,20);
         Object maxNumber = t1.FindMaximunNumber();
         Assert.assertEquals(40,maxNumber);
     }
@@ -82,35 +82,35 @@ public class TestMaximumNumber {
 
     @Test
     public void whenGivenPosition_ShouldReturn3rdPositionMaximum() {
-        FindMaximunNumber t1=new FindMaximunNumber(10,20,30);
+        FindMaximunNumber t1=new FindMaximunNumber(10,20,30,40);
         Object maxNumber = t1.FindMaximunNumber();
-        Assert.assertEquals(30,maxNumber);
+        Assert.assertEquals(40,maxNumber);
     }
 
     @Test
     public void whenGivenFloatPosition_ShouldReturn1stPositionMaximum() {
-        FindMaximunNumber t1=new FindMaximunNumber(30.1f,20.2f,10.3f);
+        FindMaximunNumber t1=new FindMaximunNumber(30.2f,20.1f,10.3f);
         Object maxNumber=t1.FindMaximunNumber();
-        Assert.assertEquals(30.1f, (Float) maxNumber,0.0);
+        Assert.assertEquals(30.2f, (Float) maxNumber,0.0);
     }
 
     @Test
     public void whenGivenFloatPosition_ShouldReturn2ndPositionMaximum() {
-        FindMaximunNumber t1=new FindMaximunNumber(30.1f,20.2f,10.3f);
+        FindMaximunNumber t1=new FindMaximunNumber(20.1f,30.2f,10.3f);
         Object maxNumber=t1.FindMaximunNumber();
-        Assert.assertEquals(30.1f, (Float) maxNumber,0.0);
+        Assert.assertEquals(30.2f, (Float) maxNumber,0.0);
     }
 
     @Test
     public void whenGivenFloatPosition_ShouldReturn3rdPositionMaximum() {
-        FindMaximunNumber t1=new FindMaximunNumber(30.1f,20.2f,10.3f);
+        FindMaximunNumber t1=new FindMaximunNumber(10.1f,20.2f,30.3f);
         Object maxNumber=t1.FindMaximunNumber();
-        Assert.assertEquals(30.1f, (Float) maxNumber,0.0);
+        Assert.assertEquals(30.3f, (Float) maxNumber,0.0);
     }
 
     @Test
     public void whenGivenStringPosition_ShouldReturn1stPositionMaximum() {
-        FindMaximunNumber t1=new FindMaximunNumber("Apple","peach","banana");
+        FindMaximunNumber t1=new FindMaximunNumber("peach","Apple","banana");
         Object maxString=t1.FindMaximunNumber();
         Assert.assertEquals("peach",maxString);
     }
@@ -124,7 +124,7 @@ public class TestMaximumNumber {
 
     @Test
     public void whenGivenStringPosition_ShouldReturn3rdPositionMaximum() {
-        FindMaximunNumber t1=new FindMaximunNumber("Apple","peach","banana");
+        FindMaximunNumber t1=new FindMaximunNumber("Apple","banana","peach");
         Object maxString=t1.FindMaximunNumber();
         Assert.assertEquals("peach",maxString);
     }
@@ -135,8 +135,5 @@ public class TestMaximumNumber {
         int maxNumber=t1.FindMaximum(30,20,10,50,70,80,100);
         Assert.assertEquals(100,maxNumber);
     }
-
-
-
 
 }
